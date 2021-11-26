49b11bbe5d7ab8f93b341ec3784098c5a9cb9f89
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-11-26 18:52:50 +0000

    Update nav links and add hero image

--- cartridge_market/settings.py
+++ cartridge_market/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$2xqez9rf&o)pnsu_nbm7(*re7l$-=&0&1c$(0e#mba=r71fkd'  # noqa: E501
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
