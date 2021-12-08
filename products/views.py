7415117f1bac407c4c3cc1a88d80beb17c2933c2
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-12-08 15:35:34 +0000

    Add search functionality

--- products/views.py
+++ products/views.py
@@ -2,7 +2,10 @@
 Views difined for the 'home' app
 """
 # pylint: disable=no-member
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
+from django.contrib import messages
+from django.db.models import Q
+
 from products.models import Game
 
 
@@ -11,9 +14,32 @@ def all_games(request):
     A view to show all games currently available on the site.
     """
     games = Game.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('all_games'))
+
+            queries = Q(
+                name__icontains=query
+            ) | Q(
+                    description__icontains=query
+            ) | Q(
+                    genre__name__icontains=query
+            ) | Q(
+                    brand__name__icontains=query
+            ) | Q(
+                    console__name__icontains=query
+            )
+            games = games.filter(queries)
 
     context = {
         'games': games,
+        'search_term': query,
     }
 
     return render(request, 'products/all_games.html', context)
