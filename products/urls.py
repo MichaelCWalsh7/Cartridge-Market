99d0cc17aa9280535df214378390d6abc6c256b5
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-12-10 15:37:15 +0000

    Add sony publisher page view & url

--- products/urls.py
+++ products/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('', views.all_games, name='all_games'),
     path('nintendo/', views.nintendo_games, name='nintendo_games'),
     path('sega/', views.sega_games, name='sega_games'),
+    path('sony/', views.sony_games, name='sony_games'),
+    path('atari/', views.atari_games, name='atari_games'),
 ]
--- products/views.py
+++ products/views.py
@@ -115,3 +115,75 @@ def sega_games(request):
     }
 
     return render(request, 'products/sega_games.html', context)
+
+
+def sony_games(request):
+    """
+    A view to show all Sony games currently available on the site.
+    """
+    games = Game.objects.filter(publisher__name="Sony")
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('all_games'))
+
+            queries = Q(
+                name__icontains=query
+            ) | Q(
+                    description__icontains=query
+            ) | Q(
+                    genre__name__icontains=query
+            ) | Q(
+                    publisher__name__icontains=query
+            ) | Q(
+                    console__name__icontains=query
+            )
+            games = games.filter(queries)
+
+    context = {
+        'games': games,
+        'search_term': query,
+    }
+
+    return render(request, 'products/sony_games.html', context)
+
+
+def atari_games(request):
+    """
+    A view to show all Atari games currently available on the site.
+    """
+    games = Game.objects.filter(publisher__name="Atari")
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('all_games'))
+
+            queries = Q(
+                name__icontains=query
+            ) | Q(
+                    description__icontains=query
+            ) | Q(
+                    genre__name__icontains=query
+            ) | Q(
+                    publisher__name__icontains=query
+            ) | Q(
+                    console__name__icontains=query
+            )
+            games = games.filter(queries)
+
+    context = {
+        'games': games,
+        'search_term': query,
+    }
+
+    return render(request, 'products/atari_games.html', context)
