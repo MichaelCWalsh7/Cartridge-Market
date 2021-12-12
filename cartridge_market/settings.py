eedf7f5cf7b46f62e29d036b094b73381124b8cd
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-12-12 18:32:13 +0000

    Fix config vars typo

--- cartridge_market/settings.py
+++ cartridge_market/settings.py
@@ -173,7 +173,7 @@ if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'mcw-cartridge-market'
     AWS_S3_REGION_NAME = 'eu-west-3'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
