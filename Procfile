5fc71a5c3397bf3b9e62268a99e176c738cc6d3e
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-12-12 16:08:24 +0000

    Deploy app to heroku #1

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn cartridge_market.wsgi:application
--- cartridge_market/settings.py
+++ cartridge_market/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os  # pylint: disable=unused-import  # noqa: F401
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mcw-cartridge-market.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -103,12 +104,17 @@ WSGI_APPLICATION = 'cartridge_market.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
