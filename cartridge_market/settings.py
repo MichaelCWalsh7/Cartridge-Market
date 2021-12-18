3c1b596068b2b36c5f144ee66d1b6f4d197289ff
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-12-18 14:24:50 +0000

    Fix media url path error 2

--- cartridge_market/settings.py
+++ cartridge_market/settings.py
@@ -181,7 +181,7 @@ if 'USE_AWS' in os.environ:
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-    MEDIAFILES_LOCATION = 'media'
+    MEDIAFILES_LOCATION = 'media/'
 
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
