da739c9cd1ec1ed4ccab2f3296ef7138f7139b92
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-12-19 17:40:47 +0000

    Add game_details links to game cards site-wide

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@
 Views difined for the 'home' app
 """
 # pylint: disable=no-member
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from products.models import Game, Publisher
 
 
@@ -10,10 +10,10 @@ def index(request):
     """
     A view to return the index page and spme featured games
     """
-    sm64 = Game.objects.all().filter(sku='FG-1')
-    crash = Game.objects.all().filter(sku='FG-2')
-    sonic = Game.objects.all().filter(sku='FG-3')
-    pac_man = Game.objects.all().filter(sku='FG-4')
+    sm64 = get_object_or_404(Game, sku='FG-1')
+    crash = get_object_or_404(Game, sku='FG-2')
+    sonic = get_object_or_404(Game, sku='FG-3')
+    pac_man = get_object_or_404(Game, sku='FG-4')
     publishers = Publisher.objects.all()
 
     context = {
