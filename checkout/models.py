d5cbb4f85f9c3eded7d8c89eef265fd8875a28cf
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2022-01-10 14:45:44 +0000

    Add checkout models

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,94 @@
-from django.db import models
+"""
+This is where the data schema and functionality for making secure purchases is
+implemented. It accounts for both registered and non-registered users.
+"""
+# pylint: disable=no-member
 
-# Create your models here.
+import uuid
+
+from django.db import models
+from django.db.models import Sum
+
+from django_countries.fields import CountryField
+
+from products.models import Game
+
+
+class Order(models.Model):
+    """
+    This is the class for an order placed on the site that users will use to
+    checkout with.
+    """
+
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
+    postcode = models.CharField(  # noqa: DJ01
+        max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(  # noqa: DJ01
+        max_length=80, null=True, blank=True)
+    county = models.CharField(  # noqa: DJ01
+        max_length=80, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    order_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0)
+
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID.
+        """
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """
+        Updates the total each time a new line is added.
+        """
+        self.order_total = self.lineitems.aggregate(
+            Sum('lineitem_total'))['lineitem_total__sum']
+        self.grand_total = self.order_total
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number if it hasn't
+        been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+
+class OrderLineItem(models.Model):
+    """
+    The class that delas with individual items in the bag, their quantity,
+    price etc.
+    """
+    order = models.ForeignKey(
+        Order, null=False, blank=False, on_delete=models.CASCADE,
+        related_name='lineitems')
+    product = models.ForeignKey(
+        Game, null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False, blank=False,
+        editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the lineitem total if it
+        hasn't been set already.
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
