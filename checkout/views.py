6cf1baf0f37cc022c1af22b3b362adee8b2d1e48
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2022-01-15 18:59:35 +0000

    Add metadata & method details to stripe instance

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,11 @@
 """
 Views defined for the 'checkout' app
 """
-# pylint: disable=no-member
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+# pylint: disable=no-member,broad-except,invalid-name
+import json
+from django.shortcuts import (render, redirect, reverse, get_object_or_404,
+                              HttpResponse)
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -12,6 +15,26 @@ from cart.contexts import cart_contents
 from products.models import Game
 from .forms import Order, OrderForm
 from .models import OrderLineItem
+
+
+@require_POST
+def cache_checkout_data(request):
+    """
+    Caches the users checkout data if they have opted in to the feature.
+    """
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'cart': json.dumps(request.session.get('cart', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be processed \
+            right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
