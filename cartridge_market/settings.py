5eed552a853695e20dd8fce3b4f1c2a8cad2e320
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2022-01-21 17:53:27 +0000

    Migrate to postgres database

--- cartridge_market/settings.py
+++ cartridge_market/settings.py
@@ -126,6 +126,7 @@ WSGI_APPLICATION = 'cartridge_market.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
+    print('loading postgres database...')
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
