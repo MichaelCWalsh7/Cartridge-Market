b5761fddf0d0574d1ae5d98164a83a5f9fafd0a3
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2022-01-21 16:01:38 +0000

    Change databases for migrations

--- cartridge_market/settings.py
+++ cartridge_market/settings.py
@@ -125,12 +125,10 @@ WSGI_APPLICATION = 'cartridge_market.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
-    print("loading postres db .....")
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
-    print("loading local db .....")
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
