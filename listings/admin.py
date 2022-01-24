ac1cac75a47fa374e018f6b42c9d8ef3d1b5c393
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2022-01-24 15:07:01 +0000

    Add admin model for listings app

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,19 @@
+"""
+Register the storefronts modules so that they can be used in site admin
+"""
 from django.contrib import admin
+from .models import Listing
 
-# Register your models here.
+
+class ListingAdmin(admin.ModelAdmin):
+    """
+    Defines the order model in the admin.
+    """
+    model = Listing
+    readonly_fields = ('user', 'storefront',)
+
+    fields = ('title', 'date', 'condition', 'price', 'copies_available',
+              'decription', 'image',)
+
+
+admin.site.register(Listing)
