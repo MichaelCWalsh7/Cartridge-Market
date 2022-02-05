dc582dd1c9a6657dcd25771fede649753e178715
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2022-02-05 13:21:06 +0000

    Finish home tests

--- home/tests.py
+++ home/tests.py
@@ -3,16 +3,102 @@ Tests the view for the home app.
 """
 # pylint: disable=no-member
 from django.test import TestCase
+from products.models import Publisher, Console, Genre, Game
 
 
 class TestHomeView(TestCase):
     """
     Tests that the home view works correctly
     """
+    def setUp(self):
+        """
+        Instantiate objects for testing.
+        """
+        self.test_publisher = Publisher.objects.create(
+            name='testPublisher',
+            image_url='test',
+            image='test',
+        )
+
+        self.test_console = Console.objects.create(
+            publisher=self.test_publisher,
+            sku='test',
+            name='testConsole',
+            description='test',
+            price=25.52,
+            image_url='test',
+            image='test',
+            release_year='1997',
+            controller_ports=2,
+        )
+
+        self.test_genre = Genre.objects.create(
+            name='Test'
+        )
+
+        self.test_game1 = Game.objects.create(
+            publisher=self.test_publisher,
+            console=self.test_console,
+            genre=self.test_genre,
+            sku='FG-1',
+            name='testGame',
+            description='test',
+            image_url='test',
+            image='test',
+            release_year='1997',
+            multiplayer=True,
+            rating=100,
+            developer='test',
+        )
+
+        self.test_game2 = Game.objects.create(
+            publisher=self.test_publisher,
+            console=self.test_console,
+            genre=self.test_genre,
+            sku='FG-2',
+            name='testGame',
+            description='test',
+            image_url='test',
+            image='test',
+            release_year='1997',
+            multiplayer=True,
+            rating=100,
+            developer='test',
+        )
+
+        self.test_game3 = Game.objects.create(
+            publisher=self.test_publisher,
+            console=self.test_console,
+            genre=self.test_genre,
+            sku='FG-3',
+            name='testGame',
+            description='test',
+            image_url='test',
+            image='test',
+            release_year='1997',
+            multiplayer=True,
+            rating=100,
+            developer='test',
+        )
+
+        self.test_game4 = Game.objects.create(
+            publisher=self.test_publisher,
+            console=self.test_console,
+            genre=self.test_genre,
+            sku='FG-4',
+            name='testGame',
+            description='test',
+            image_url='test',
+            image='test',
+            release_year='1997',
+            multiplayer=True,
+            rating=100,
+            developer='test',
+        )
+
     def test_home_view_url(self):
         """
         Tests that the home page page URL exists
-
         """
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
