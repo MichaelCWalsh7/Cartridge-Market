1d5c6041a5f87ed1589ce9836f8ffbf0fa333132
MichealCWalsh <MichaelCWalsh792@gmail.com>
2022-11-26 19:44:04 +0000

    Change debug in settings

--- cartridge_market/settings.py
+++ cartridge_market/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['mcw-cartridge-market.herokuapp.com', 'localhost']
 # Add Render.com URL to allowed hosts
