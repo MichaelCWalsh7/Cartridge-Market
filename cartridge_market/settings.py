452d521bd87feb8498cbdf54de302cb3ee0ae911
MichealCWalsh <MichaelCWalsh792@gmail.com>
2022-11-26 18:40:10 +0000

    Fix static url error 1

--- cartridge_market/settings.py
+++ cartridge_market/settings.py
@@ -199,7 +199,7 @@ if 'USE_AWS' in os.environ:
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    STATICFILES_LOCATION = 'static'
+    STATICFILES_LOCATION = 'static/'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media/'
 
