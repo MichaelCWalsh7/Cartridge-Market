97210a47124bc53831af2937a2a0fdd94ebcda3f
MichealCWalsh <MichaelCWalsh792@gmail.com>
2022-11-26 17:45:16 +0000

    Begin migration to Render

--- cartridge_market/settings.py
+++ cartridge_market/settings.py
@@ -27,6 +27,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 DEBUG = True
 
 ALLOWED_HOSTS = ['mcw-cartridge-market.herokuapp.com', 'localhost']
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
